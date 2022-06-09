ceaf9bbcb8ebee6bab16c07b29d7f7ffbfd9648a
PRADIPTA KUMAR PARIDA <pradipta18.parida@gmail.com>
2022-06-09 11:34:31 +0530

    All Done

--- src/JavaScript_Bootcamp/data_types.js
+++
@@ -1,3 +0,0 @@
-/* Data Types :
-undefined, null, boolean, string, symbol, number and object.
-*/
--- src/JavaScript_Bootcamp/intro.js
+++
@@ -1,4 +0,0 @@
-var number=7; //this is a in-line comment.
-
-/* this is a 
-multi-line comment in java-script. */
