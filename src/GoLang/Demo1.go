4d7c11a84720120d3ac32663502a9c53e7157420
“pradipta” <pradipta18.parida@gmail.com>
2022-07-04 15:15:33 +0530

    intriguing

---
+++ src/GoLang/Demo1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := "Kumar"
+	b := "Parida"
+	c := "Kumar"
+
+	result := a == b
+	result1 := a == c
+
+	fmt.Println(result)
+	fmt.Println(result1)
+
+}
---
+++ src/GoLang/Demo3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		//for loop
+		for i := 1; i <= 10; i++ {
+			fmt.Println("Pradipta")
+			fmt.Println(i)
+	*/
+
+	//for loop as while loop
+	num := 1
+	for num <= 10 {
+		fmt.Println(num)
+		num++
+	}
+
+}
