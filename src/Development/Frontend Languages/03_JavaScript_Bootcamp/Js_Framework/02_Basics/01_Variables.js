672f00f76e5d6ace05abd9af1879dd6878b5aceb
“pradipta” <pradipta18.parida@gmail.com>
2022-12-26 20:39:22 +0530

    hyuuu

---
+++ src/Development/Frontend Languages/03_JavaScript_Bootcamp/Js_Framework/02_Basics/01_Variables.js
@@ -0,0 +1,11 @@
+// const keyword can't be changed
+const accountId = 14354
+
+let accountEmail = "pradipta28.parida@gmail.com"
+
+var accountPass = "12345"
+
+accountCity = "Bhubaneswar"
+
+console.log(accountId);
+
