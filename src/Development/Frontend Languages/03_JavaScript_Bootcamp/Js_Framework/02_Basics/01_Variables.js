4ee16860a151df1d7c5209dc0d2bd0ce2e1a637b
“pradipta” <pradipta18.parida@gmail.com>
2022-12-27 23:38:10 +0530

    fff

--- src/Development/Frontend Languages/03_JavaScript_Bootcamp/Js_Framework/02_Basics/01_Variables.js
+++ src/Development/Frontend Languages/03_JavaScript_Bootcamp/Js_Framework/02_Basics/01_Variables.js
@@ -1,11 +1,38 @@
-// const keyword can't be changed
+/* 
+
+In, VS code we can use ctrl + / to comment any JS code.
+
+; is optional at the end of line of JS code but its a best practice to follow
+
+*/
+
 const accountId = 14354
+/* accountId = 5
+const keyword can't be changed as its constant */
 
+
+/*
+let and var keyword are used for variables.
+
+var is now been phased out and no longer in use of todays modern js world.
+so,prefer not to use var because of issue in block scope and functional scope
+*/
 let accountEmail = "pradipta28.parida@gmail.com"
+accountEmail = "ghp@gmail.com"
 
-var accountPass = "12345"
+// JScript is a very loose language, we can declare a variable without any keyword, but it isn't a best practice to do it
+
+let accountState; // If you don't declare value to variable then the default value is Undefined
+
+var accountPassword = "12345"
+accountPassword = "22222"
+ 
 
 accountCity = "Bhubaneswar"
+accountCity = "Kolkata"
 
-console.log(accountId);
+// To display only one value into the console
+console.log(accountId); 
 
+//To display multiple values into the console
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ src/Development/Frontend Languages/03_JavaScript_Bootcamp/Js_Framework/02_Basics/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all JS code as newer version.By default, it's used but its optional to mention it
+
+// alert(3+3) // we are using nodejs, not browser
+
+let age = 34.88;
+
+/*
+Data Types in JS;
+Number => 
+bigint =>
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value not assigned to a variable
+symbol => unique
+*/
+
+// Displays type of any value or variable in console
+console.log(typeof ("pradipta"));
+console.log(typeof (age));
+console.log(typeof (null)); // object
+console.log(typeof (undefined)); // undefined
